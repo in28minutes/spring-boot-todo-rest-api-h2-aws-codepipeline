0eb9abc2187be3a0ccd8b0fd2d858fd77d2ceb04
Ranga Rao Karanam <in28minutes@gmail.com>
2019-07-08 12:17:00 +0530

    helloworld update

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -10,7 +10,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World";
+		return "Hello World v2";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
